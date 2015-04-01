7a6584816c9d3125baa701563674b521ce9a4040
zvenshy <zvenshy@gmail.com>
2015-04-01 21:24:23 +0800

    test！

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var qunit = require('gulp-qunit');
 
 gulp.task('test', function () {
-    gulp.src('./test/index.html')
+    return gulp.src('./test/index.html')
         .pipe(qunit());
 });
